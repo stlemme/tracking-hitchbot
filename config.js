faa94ed7450e5120ef660e4ac8e9dd738ee87d33
Stefan Lemme <stefan.lemme@dfki.de>
2015-02-16 09:38:43 +0100

    initial version

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var global = {
+
+	"api": {
+		"hitchbot": "http://hitchbotimg.blob.core.windows.net/hbjs/testLocationsde10.js",
+		"img_tiles": [
+			"http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
+			"http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
+			"http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+		]
+	}
+};
